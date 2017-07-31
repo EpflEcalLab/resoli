76e72c7a7ce563b8ad7f1dce857e60f276ece4f9
Toni Fisler <mail@tonifisler.ch>
2017-07-31 17:50:42 +0200

    setup menu and animations

---
+++ web/themes/quartiers_solidaires/assets/components/molecules/nav/nav.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+
+const nav = () => {
+  $(function() {
+    const toggleMenu = () => {
+      $('.navbar-toggle').toggleClass('active');
+      $('body').toggleClass('open');
+    };
+    toggleMenu();
+
+    // Handle click on menu toggle button
+    $(
+      '.navbar-container'
+    ).on('click', '.navbar-toggle, .navbar-overlay', function() {
+      toggleMenu();
+    });
+
+    // Close menu on Escape key
+    $(document).keyup(function(e) {
+      if (
+        e.keyCode === 27 &&
+        $('body').hasClass('open') &&
+        $('body').hasClass('open')
+      )
+        toggleMenu();
+    });
+  });
+};
+
+export default nav;

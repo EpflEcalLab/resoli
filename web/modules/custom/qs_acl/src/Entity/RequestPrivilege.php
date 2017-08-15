1470f0886d0fcb3bf9b8e2eea1c9a395091d3836
Kevin Wenger <wenger.kev@gmail.com>
2017-08-15 14:00:48 +0200

    create entity to manage privilege requests

---
+++ web/modules/custom/qs_acl/src/Entity/RequestPrivilege.php
@@ -0,0 +1,282 @@
+<?php
+
+namespace Drupal\qs_acl\Entity;
+
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\user\UserInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Defines the Request Privilege entity.
+ *
+ * Populate and updated when a user request new privilege on an Entity such:
+ *   - Community (Taxonomy Term), to became Member.
+ *   - Activity (Node), to became Member.
+ *
+ * The following construct is the actual definition of the entity type which
+ * is read and cached. Don't forget to clear cache after changes.
+ *
+ * @ContentEntityType(
+ *   id = "request_privileges",
+ *   label = @Translation("Request Privilege"),
+ *   base_table = "request_privileges",
+ *   admin_permission = "administer request_privilege entity",
+ *   fieldable = false,
+ *   entity_keys = {
+ *     "id" = "id"
+ *   },
+ * )
+ */
+class RequestPrivilege extends ContentEntityBase implements ContentEntityInterface, EntityChangedInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getChangedTime() {
+    return $this->get('changed')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setChangedTime($timestamp) {
+    $this->set('changed', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getChangedTimeAcrossTranslations() {
+    $changed = $this->getUntranslated()->getChangedTime();
+    foreach ($this->getTranslationLanguages(FALSE) as $language) {
+      $translation_changed = $this->getTranslation($language->getId())->getChangedTime();
+      $changed = max($translation_changed, $changed);
+    }
+    return $changed;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getReviewedTime() {
+    return $this->get('reviewed')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setReviewedTime($timestamp) {
+    $this->set('reviewed', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('user');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('user', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPrivilege() {
+    return $this->get('privilege');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPrivilege($privilege) {
+    // Load services. No way to inject them on EntityInterface.
+    $field_manger = \Drupal::service('entity_field.manager');
+
+    // Load the field definitions.
+    $bundle_fields = $field_manger->getFieldDefinitions('request_privileges', 'request_privileges');
+    $field_definition = $bundle_fields['privilege'];
+    $allowed_values = $field_definition->getSetting('allowed_values');
+
+    // Set data only if allowed values.
+    if (isset($allowed_values[$privilege])) {
+      $this->set('privilege', $privilege);
+    }
+
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBundle() {
+    return $this->get('bundle');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setBundle($bundle) {
+    // Load services. No way to inject them on EntityInterface.
+    $field_manger = \Drupal::service('entity_field.manager');
+
+    // Load the field definitions.
+    $bundle_fields = $field_manger->getFieldDefinitions('request_privileges', 'request_privileges');
+    $field_definition = $bundle_fields['bundle'];
+    $allowed_values = $field_definition->getSetting('allowed_values');
+
+    // Set data only if allowed values.
+    if (isset($allowed_values[$bundle])) {
+      $this->set('bundle', $bundle);
+    }
+
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEntity() {
+    return $this->get('entity');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setEntity(EntityInterface $entity) {
+    $this->set('entity', $entity);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * How to humanize the boolean status:
+   *  - pending: status = 0 & reviewer = NULL
+   *  - accepted: status = 1 & reviewer != NULL
+   *  - declined: status = 0 & reviewer != NULL.
+   */
+  public function getStatus() {
+    return $this->get('status');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setStatus($status) {
+    $this->set('status', $status);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getReviewer() {
+    return $this->get('reviewer');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setReviewer(UserInterface $account) {
+    $this->set('reviewer', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(new TranslatableMarkup('ID'))
+      ->setDescription(new TranslatableMarkup('The ID of the request for privilege.'))
+      ->setReadOnly(TRUE);
+
+    $fields['privilege'] = BaseFieldDefinition::create('list_string')
+      ->setLabel(new TranslatableMarkup('Privilege'))
+      ->setDescription(new TranslatableMarkup('The requested privilege.'))
+      ->addConstraint('UniqueField')
+      ->setRequired(TRUE)
+      ->setSettings([
+        'allowed_values' => [
+            // Communities bundle privilege.
+          'community_managers'   => new TranslatableMarkup('Manage of community'),
+          'community_organizers' => new TranslatableMarkup('Organizer of community'),
+          'community_members'    => new TranslatableMarkup('Member of community'),
+            // Activity bundle privilege.
+          'activity_organizers'  => new TranslatableMarkup('Manage of activity'),
+          'activity_maintainers' => new TranslatableMarkup('Organizer of activity'),
+          'activity_members'     => new TranslatableMarkup('Member of activity'),
+        ],
+      ]);
+
+    $fields['bundle'] = BaseFieldDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Bundle'))
+      ->setDescription(new TranslatableMarkup('The bundle this privilege is attached (community or activity).'))
+      ->setRequired(TRUE)
+      ->setSettings([
+        'allowed_values' => [
+          'communities' => new TranslatableMarkup('Communities'),
+          'activity'    => new TranslatableMarkup('Activity'),
+        ],
+      ]);
+
+    $fields['entity'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(new TranslatableMarkup('Entity'))
+      ->setDescription(new TranslatableMarkup('The entity ID this privilege is attached.'))
+      ->setRequired(TRUE);
+
+    $fields['user'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(new TranslatableMarkup('Used by'))
+      ->setDescription(new TranslatableMarkup('The user which request the privilege.'))
+      ->setRequired(TRUE)
+      ->setSetting('target_type', 'user');
+
+    $fields['reviewer'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(new TranslatableMarkup('Reviewer'))
+      ->setDescription(new TranslatableMarkup('The user which operate the review.'))
+      ->setSetting('target_type', 'user');
+
+    $fields['status'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(new TranslatableMarkup('Request status'))
+      ->setDescription(new TranslatableMarkup('A boolean indicating whether the request is pending|accepted|declined.'))
+      ->setRequired(TRUE)
+      ->setDefaultValue(FALSE);
+
+    $fields['reviewed'] = BaseFieldDefinition::create('timestamp')
+      ->setLabel(new TranslatableMarkup('Reviewed'))
+      ->setDescription(new TranslatableMarkup('The time that the request has been reviewed.'));
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(new TranslatableMarkup('Created'))
+      ->setDescription(new TranslatableMarkup('The time that the entity was created.'));
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(new TranslatableMarkup('Changed'))
+      ->setDescription(new TranslatableMarkup('The time that the entity was last edited.'));
+
+    return $fields;
+  }
+
+}

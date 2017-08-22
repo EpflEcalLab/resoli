07ad8fe7cc0f479aaf4d816cb92b5daf6ce8c82a
Kevin Wenger <wenger.kev@gmail.com>
2017-08-22 14:58:24 +0200

    add communities selection on login if multiple communities

--- web/modules/custom/qs_auth/src/Controller/AccountController.php
+++ web/modules/custom/qs_auth/src/Controller/AccountController.php
@@ -3,12 +3,59 @@
 namespace Drupal\qs_auth\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\qs_acl\Service\AccessControl;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\taxonomy\TermInterface;
 
 /**
  * AccountController.
  */
 class AccountController extends ControllerBase {
+
+  /**
+   * Access Control Service.
+   *
+   * @var \Drupal\qs_acl\Service\AccessControl
+   */
+  protected $acl;
+
+  /**
+   * The term Storage.
+   *
+   * @var \Drupal\taxonomy\TermStorageInterface
+   */
+  private $termStorage;
+
+  /**
+   * The entity query factory.
+   *
+   * @var \Drupal\Core\Entity\Query\QueryFactory
+   */
+  protected $queryFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(AccessControl $acl, EntityTypeManagerInterface $entity_type_manager, QueryFactory $query_factory) {
+    $this->acl          = $acl;
+    $this->termStorage  = $entity_type_manager->getStorage('taxonomy_term');
+    $this->queryFactory = $query_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+    // Load customs services used in this class.
+    $container->get('qs_acl.access_control'),
+    $container->get('entity_type.manager'),
+    $container->get('entity.query')
+    );
+  }
 
   /**
    * Approval page.
@@ -35,7 +82,24 @@ class AccountController extends ControllerBase {
    * status of pending appliance & membership.
    */
   public function communities() {
-    return ['#markup' => 'communities'];
+    $variables['communities'] = $this->acl->getCommunities();
+    $variables['pending'] = $this->acl->getPendingApprovalCommunities();
+
+    return [
+      '#theme'     => 'qs_auth_communities_page',
+      '#variables' => $variables,
+      '#cache' => [
+        'contexts' => [
+          'user',
+        ],
+        'tags' => [
+          // Invalidated whenever any Community is updated, deleted or created.
+          'taxonomy_term_list:communities',
+          // Invalidated whenever any Privilege is updated, deleted or created.
+          'privilege_list:privilege',
+        ],
+      ],
+    ];
   }
 
 }

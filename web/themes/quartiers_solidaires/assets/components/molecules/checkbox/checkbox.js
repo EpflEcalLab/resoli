8d3e42de129863efe46cb7a29414c2cf5e2e2bc5
Toni Fisler <mail@tonifisler.ch>
2017-09-01 11:40:58 +0200

    fix check all button active state

--- web/themes/quartiers_solidaires/assets/components/molecules/checkbox/checkbox.js
+++ web/themes/quartiers_solidaires/assets/components/molecules/checkbox/checkbox.js
@@ -5,13 +5,11 @@ const checkbox = () => {
   // themes.
   const $btn_check_all = $('#js-all-themes');
   const $themes = $('.js-filter-themes .btn.checkbox');
-  let checked = $btn_check_all.prop('checked');
 
   // Listen to click events on the ALL button
   // We can't listen to `change` event as it would trigger every time
   $btn_check_all.on('click', function() {
-    checked = !checked;
-    toggleSelected(checked);
+    toggleSelected(!$btn_check_all.find('input[type=checkbox]').prop('checked'));
   });
 
   // We can't listen to `change` event as it would trigger every time
@@ -20,10 +18,14 @@ const checkbox = () => {
     // wether the clicked element will be checked
     const selfChecked = !$(this).find('input[type=checkbox]').prop('checked');
     // how many options are currently checked
-    const checkedOptions = $('.js-filter-themes').find('input[type=checkbox]:checked');
+    const checkedOptions = $themes.find('input[type=checkbox]:checked');
     // total of checked options after the change event triggers
-    let counter = selfChecked ? checkedOptions.length + 1 : checkedOptions.length - 1;
+    const counter = selfChecked ? checkedOptions.length + 1 : checkedOptions.length - 1;
 
+    toggleAllBtn(counter);
+  });
+
+  const toggleAllBtn = (counter) => {
     // if counter is equal to the total of checkboxes, toggle the ALL button
     // else if counter is less than the total and the ALL button is active, toggle it
     if (counter >= $themes.length) {
@@ -31,7 +33,7 @@ const checkbox = () => {
     } else if (counter < $themes.length && $btn_check_all.hasClass('active')) {
       $btn_check_all.button('toggle');
     }
-  });
+  }
 
   const toggleSelected = (state) => {
     // Check all checkboxes that are not yet of the same value as ALL button
@@ -42,6 +44,9 @@ const checkbox = () => {
       }
     });
   }
-};
+
+  // We need to check on load if the button has to be enabled
+  toggleAllBtn($themes.find('input[type=checkbox]:checked').length);
+}
 
 export default checkbox;

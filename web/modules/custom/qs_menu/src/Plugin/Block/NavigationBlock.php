946ec1297866b2696c8e1c99f4de4b98edc4f9d5
Kevin Wenger <wenger.kev@gmail.com>
2017-09-04 19:39:05 +0200

    fix menu when dashboard events

--- web/modules/custom/qs_menu/src/Plugin/Block/NavigationBlock.php
+++ web/modules/custom/qs_menu/src/Plugin/Block/NavigationBlock.php
@@ -101,6 +101,11 @@ class NavigationBlock extends BlockBase implements ContainerFactoryPluginInterfa
       $community = $node->field_community->entity;
     }
 
+    $event = $this->route->getParameter('event');
+    if (!$community && $event && $event->bundle() == 'event') {
+      $community = $event->field_activity->entity->field_community->entity;
+    }
+
     // When the community doesn't exists, it's impossible build the menu.
     if (!$community) {
       return $render;

a64506ef0ec68355b36742d93a3921803f688290
Kevin Wenger <wenger.kev@gmail.com>
2017-10-20 11:10:58 +0200

    refactoring approval page

--- web/modules/custom/qs_auth/src/Controller/AccountController.php
+++ web/modules/custom/qs_auth/src/Controller/AccountController.php
@@ -73,50 +73,6 @@ class AccountController extends ControllerBase {
   }
 
   /**
-   * Checks access for Approval.
-   *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   Run access checks for this account.
-   * @param \Drupal\taxonomy\TermInterface $community
-   *   Run access checks for this taxonomy.
-   *
-   * @return \Drupal\Core\Access\AccessResultInterface
-   *   The access result.
-   */
-  public function accessApproval(AccountInterface $account, TermInterface $community) {
-    $access = AccessResult::forbidden();
-    if ($community->bundle() == 'communities') {
-      $access = AccessResult::allowed();
-    }
-    return $access;
-  }
-
-  /**
-   * Approval page.
-   *
-   * This page is shown when the user access to a community which he previously
-   * applied but which he's not a certified member.
-   * He must be reviewed by a Manager of this community.
-   */
-  public function approval(TermInterface $community) {
-    $variables['community'] = $community;
-
-    return [
-      '#theme' => 'qs_auth_approval_page',
-      '#variables' => $variables,
-      '#cache' => [
-        'contexts' => [
-          'user',
-        ],
-        'tags' => [
-          // Invalidated whenever any Privilege is updated, deleted or created.
-          'privilege_list:privilege',
-        ],
-      ],
-    ];
-  }
-
-  /**
    * Communities page.
    *
    * This page is shown when the user has more than 1 community where he's a

ea809017cca6fa73c058ecaf4fb3fbc08eaa4922
Kevin Wenger <wenger.kev@gmail.com>
2017-10-26 15:45:56 +0200

    add list of photo by month

---
+++ web/modules/custom/qs_photo/src/Controller/CollectionController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Drupal\qs_photo\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\taxonomy\TermInterface;
+use Drupal\qs_acl\Service\AccessControl;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\qs_photo\Service\PhotoManager;
+use Drupal\qs_calendar\Service\CalendarBuilder;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Datetime\DrupalDateTime;
+
+/**
+ * CollectionController.
+ */
+class CollectionController extends ControllerBase {
+
+  /**
+   * Access Control Service.
+   *
+   * @var \Drupal\qs_acl\Service\AccessControl
+   */
+  private $acl;
+
+  /**
+   * The entity QS Photo Manager.
+   *
+   * @var \Drupal\qs_photo\Service\PhotoManager
+   */
+  protected $photoManager;
+
+  /**
+   * The Calendar builder.
+   *
+   * @var \Drupal\qs_calendar\Service\CalendarBuilder
+   */
+  protected $calendarBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(AccessControl $acl, PhotoManager $photo_manager, CalendarBuilder $calendar_builder) {
+    $this->acl             = $acl;
+    $this->photoManager    = $photo_manager;
+    $this->calendarBuilder = $calendar_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+    // Load customs services used in this class.
+      $container->get('qs_acl.access_control'),
+      $container->get('qs_photo.photo_manager'),
+      $container->get('qs_calendar.calendar_builder')
+    );
+  }
+
+  /**
+   * Checks access.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   * @param \Drupal\taxonomy\TermInterface $community
+   *   Run access checks for this taxonomy.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function access(AccountInterface $account, TermInterface $community) {
+    $access = AccessResult::forbidden();
+
+    if ($this->acl->hasAccessCommunity($community)) {
+      $access = AccessResult::allowed();
+    }
+
+    return $access;
+  }
+
+  /**
+   * Collection by months.
+   */
+  public function months(Request $request, TermInterface $community) {
+    $variables = ['community' => $community];
+
+    // Get pagination month.
+    $pagination_month = $request->query->get('month');
+
+    $month = new DrupalDateTime();
+    if ($pagination_month) {
+      try {
+        $month = DrupalDateTime::createFromFormat('Y-m-01', $pagination_month);
+      }
+      catch (\Exception $e) {
+        $month = new DrupalDateTime();
+      }
+    }
+
+    $date_start         = $this->calendarBuilder->getFirstMondayMonthFullWeek($month);
+    $date_end           = $this->calendarBuilder->getLastSundayMonthFullWeek($month);
+    $variables['dates'] = $this->calendarBuilder->build($date_start, $date_end);
+
+    $date_end->setTime(23, 59, 59);
+
+    // Get all photos for the given month ordered by event end date.
+    $variables['photos'] = $this->photoManager->getByDate($community, $date_start, $date_end);
+
+    return [
+      '#theme'     => 'qs_photo_collection_by_month_page',
+      '#variables' => $variables,
+      '#cache' => [
+        'contexts' => [
+          'user',
+          'url.query_args',
+        ],
+        'tags' => [
+          'tags' => $this->getCacheTags($variables['photos']),
+        ],
+      ],
+    ];
+  }
+
+  /**
+   * Collection by themes.
+   */
+  public function themes(Request $request, TermInterface $community) {
+    $variables = [];
+    return [
+      '#theme'     => 'qs_photo_collection_by_theme_page',
+      '#variables' => $variables,
+      '#cache' => [
+        'contexts' => [
+          'user',
+          'url.query_args',
+        ],
+        'tags' => $this->getCacheTags($variables['events']),
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags(array $nodes = NULL) {
+    $tags = [
+      // Invalidated whenever any Event is updated, deleted or created.
+      'node_list:photo',
+      // Invalidated whenever any Community is updated, deleted or created.
+      'taxonomy_term_list:communities',
+      // Invalidated whenever any Privilege is updated, deleted or created.
+      'privilege_list:privilege',
+    ];
+    if ($nodes) {
+      foreach ($nodes as $node) {
+        $tags[] = 'node:' . $node->id();
+      }
+    }
+    return $tags;
+  }
+
+}

c186fca05b54519641982eff8b26dfd2d2b5067f
Kevin Wenger <wenger.kev@gmail.com>
2017-10-27 18:33:00 +0200

    fix quality control

--- web/modules/custom/qs_photo/src/Controller/CollectionController.php
+++ web/modules/custom/qs_photo/src/Controller/CollectionController.php
@@ -140,7 +140,7 @@ class CollectionController extends ControllerBase {
           'url.query_args',
         ],
         'tags' => [
-          // TODO this crashes if you don't have any photos to display
+          // TODO this crashes if you don't have any photos to display.
           'tags' => $this->getCacheTags($variables['photos']),
         ],
       ],

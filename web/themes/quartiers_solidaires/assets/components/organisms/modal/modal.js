5397b39388defe709f5963abc89129d6c51c4212
Toni Fisler <mail@tonifisler.ch>
2017-12-01 16:20:04 -0500

    fix buttons and modal fullpage

--- web/themes/quartiers_solidaires/assets/components/organisms/modal/modal.js
+++ web/themes/quartiers_solidaires/assets/components/organisms/modal/modal.js
@@ -4,7 +4,14 @@ const modal = () => {
       const $modalFooters = $('.modal-body .modal-footer');
 
       $modalFooters.each(function() {
-        $(this).closest('.modal-body').after($(this));
+        const _this = $(this);
+        // Append the modal-footer after the modal-body
+        _this.closest('.modal-body').after(_this);
+
+        // Send the form when cilcking the submit (it's now outside the form)
+        _this.on('click', '[type=submit]', function() {
+          _this.closest('.modal').find('form').submit();
+        })
       });
     });
   })(jQuery);

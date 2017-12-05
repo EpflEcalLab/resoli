1e53c7bb72e9fe9bab587afcf7befb5e55d2a79c
Kevin Wenger <wenger.kev@gmail.com>
2017-12-05 17:06:50 -0500

    fix modal submit with multi-submit handler

--- web/themes/quartiers_solidaires/assets/components/organisms/modal/modal.js
+++ web/themes/quartiers_solidaires/assets/components/organisms/modal/modal.js
@@ -1,16 +1,19 @@
 const modal = () => {
   (function ($) {
     $(document).on('DOMNodeInserted', function() {
-      const $modalFooters = $('.modal-body .modal-footer');
+      const $modalFooters = $('.modal-body .modal-footer:not(.cloned-footer)');
 
       $modalFooters.each(function() {
         const _this = $(this);
+        const clone = _this.clone();
         // Append the modal-footer after the modal-body
-        _this.removeClass('row').closest('.modal-body').after(_this);
+        _this.addClass('cloned-footer d-none').removeClass('row').closest('.modal-body').after(clone);
 
-        // Send the form when cilcking the submit (it's now outside the form)
-        _this.on('click', '[type=submit]', function() {
-          _this.closest('.modal').find('form').submit();
+        // Send the form when clicking the submit (it's now outside the form)
+        clone.on('click', '[type=submit]', function() {
+          // We have to clone & click instead of form.submit to make it works
+          // with Drupal multi-submit handlers.
+          clone.closest('.modal').find(`form #${$(this).attr('id')}`).click();
         })
       });
     });

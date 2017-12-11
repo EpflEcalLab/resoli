46515db921db392d1d016f6c636d589d941f8efd
Kevin Wenger <wenger.kev@gmail.com>
2017-12-11 10:57:56 -0500

    fix #398 - multi-submit with outside/inside submission

--- web/themes/quartiers_solidaires/assets/components/organisms/modal/modal.js
+++ web/themes/quartiers_solidaires/assets/components/organisms/modal/modal.js
@@ -1,6 +1,6 @@
 const modal = () => {
   (function ($) {
-    $(document).on('DOMNodeInserted', function() {
+    $(window).on('load', function() {
       const $modalFooters = $('.modal-body .modal-footer:not(.cloned-footer)');
 
       $modalFooters.each(function() {
@@ -9,11 +9,13 @@ const modal = () => {
         // Append the modal-footer after the modal-body
         _this.addClass('cloned-footer d-none').removeClass('row').closest('.modal-body').after(clone);
 
-        // Send the form when clicking the submit (it's now outside the form)
+        // Send the form when clicking the submit only if it's now outside the form.
         clone.on('click', '[type=submit]', function() {
           // We have to clone & click instead of form.submit to make it works
           // with Drupal multi-submit handlers.
-          clone.closest('.modal').find(`form #${$(this).attr('id')}`).click();
+          if (clone.parent('form').length == 0) {
+            clone.closest('.modal').find(`form #${$(this).attr('id')}`).click();
+          }
         })
       });
     });

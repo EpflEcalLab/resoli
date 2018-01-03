1e830efdfa63755b0f107208312b2cf877b11da1
Kevin Wenger <wenger.kev@gmail.com>
2018-01-03 16:01:26 +0100

    add new twig extension to render form with custom form_id

---
+++ web/modules/custom/qs_site/src/TwigExtension/Render.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\qs_site\TwigExtension;
+
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Provides some renderer as Twig Extensions.
+ */
+class Render extends \Twig_Extension {
+  use ContainerAwareTrait;
+
+  /**
+   * List of all Twig functions.
+   */
+  public function getFunctions() {
+    return [
+      new \Twig_SimpleFunction('qs_site_render_form', [$this, 'renderForm'], ['is_safe' => ['html']]),
+    ];
+  }
+
+  /**
+   * Unique identifier for this Twig extension.
+   */
+  public function getName() {
+    return 'qs_site.twig.render';
+  }
+
+  /**
+   * Provides an interface for form building and processing.
+   *
+   * @return \Drupal\Core\Form\FormBuilderInterface
+   *   Return the interface for form building and processing.
+   */
+  protected function getFormBuilder() {
+    return $this->container->get('form_builder');
+  }
+
+  /**
+   * Load a given form with or whitout parameters.
+   *
+   * @param string $module
+   *   The module name where the form below.
+   * @param string $form
+   *   The form class name.
+   * @param array $params
+   *   (optional) An array of parameters passed to the form.
+   * @param string $form_id
+   *   (optional) The form unique ID.
+   *
+   * @return null|array
+   *   A render array for the form or NULL if the form does not exist.
+   */
+  public function renderForm($module, $form, array $params = [], $form_id = NULL) {
+    $class = 'Drupal\\' . $module . '\\Form\\' . $form;
+    $form = new $class($form_id);
+    return $this->getFormBuilder()->getForm($form, $params);
+  }
+}

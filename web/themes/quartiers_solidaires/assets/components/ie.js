f01551b4b9fba6cbb1d96b6f458af85cd3f91870
Toni Fisler <mail@tonifisler.ch>
2018-02-15 09:41:16 +0100

    fix flags on IE #534

--- web/themes/quartiers_solidaires/assets/components/ie.js
+++ web/themes/quartiers_solidaires/assets/components/ie.js
@@ -1,35 +1,33 @@
 const ie = () => {
   (function ($) {
-    // This will fix the flag icons
-    const $flags_use = $('.flag use:last-child');
-
-    $flags_use.each(function() {
-      const transform = $(this).css('transform');
-      if (transform !== 'none') {
-        let values = transform.replace(/[^0-9\-.,]/g, '').split(',');
-        const x = values[0];
-        const y = values[3];
-
-        // origin x
-        values[4] = x * 3;
-        // origin y
-        values[5] = y * 3;
-
-
-        $(this).attr('transform', `matrix(${values.join(',')})`);
-        console.log(`matrix(${values.join(',')})`);
-      }
-    });
-
     // Detect IE like a boss
     // https://stackoverflow.com/a/21660466/1722653
     const ua = navigator.userAgent;
     const doc = $('html');
+    let ie = false;
 
     if ((ua.match(/MSIE 10.0/i))) {
       doc.addClass('ie10');
+      ie = true;
     } else if ((ua.match(/rv:11.0/i))) {
       doc.addClass('ie11');
+      ie = true;
+    }
+
+    // This will fix the flag icons
+    if (ie) {
+      const $flags_use = $('.flag use:last-child');
+
+      $flags_use.each(function() {
+        const transform = $(this).css('transform');
+        if (transform !== 'none') {
+          let values = transform.replace(/[^0-9\-.,]/g, '').split(',');
+          const x = values[0];
+          const y = values[3];
+
+          $(this).attr('transform', `scale(${x},${y}) translate(15,15)`);
+        }
+      });
     }
   })(jQuery);
 };

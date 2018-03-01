8218715cf9993f59272a8aeb4300456f30336a9c
Kevin Wenger <wenger.kev@gmail.com>
2018-03-01 11:00:37 +0100

    improve stability of 'My Photos' - progress on #576

--- features/photos/qs.photos.my-photos.feature
+++ features/photos/qs.photos.my-photos.feature
@@ -4,6 +4,46 @@ Feature: Dashboard my photos - Activity within the user can manage photos
   I want to make sure the correct activities writable are shown according ACL
 
   @api
+  Scenario: Logged as Admin, When I access "My photos" in Lausanne
+    Given I am logged in as user "admin"
+    When I am on "/photos/1/user/1"
+    And the response status code should be 200
+    Then I should see 4 ".card-list .card-list-item" elements
+    Then I should see a "#activity2" element
+    Then I should see a "#activity5" element
+    Then I should see a "#activity7" element
+    Then I should see a "#activity3" element
+    Then I should not see a "#activity4" element
+    Then I should not see a "#activity6" element
+    Then I should not see a "#activity8" element
+    Then I should not see a "#activity12" element
+    Then I should not see a "#activity13" element
+    Then I should not see a "#activity14" element
+    And I should see "qs.photos.add_photos"
+    And I should not see "qs.photos.user.collection.empty"
+
+  @api
+  Scenario: Logged as Admin, When I access "My photos" in Fribourg
+    Given I am logged in as user "admin"
+    When I am on "/photos/2/user/1"
+    And the response status code should be 200
+    Then I should see 0 ".card-list .card-list-item" elements
+    Then I should not see a "#activity11" element
+    And I should see "qs.photos.add_photos"
+    And I should not see "qs.photos.user.collection.empty"
+
+  @api
+  Scenario: Logged as Admin, When I access "My photos" in Genève
+    Given I am logged in as user "admin"
+    When I am on "/photos/3/user/1"
+    And the response status code should be 200
+    Then I should see 1 ".card-list .card-list-item" elements
+    Then I should see a "#activity9" element
+    Then I should not see a "#activity10" element
+    And I should see "qs.photos.add_photos"
+    And I should not see "qs.photos.user.collection.empty"
+
+  @api
   Scenario: Logged as Member of Fribourg, When I access "My photos"
     Given I am logged in as user "member+fribourg+approval+lausanne"
     When I am on "/photos/2/user/11"
--- web/modules/custom/qs_photo/src/Controller/UserController.php
+++ web/modules/custom/qs_photo/src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\qs_acl\Service\AccessControl;
 use Drupal\Core\Access\AccessResult;
 use Drupal\qs_activity\Service\ActivityManager;
+use Drupal\qs_photo\Service\PhotoManager;
 use Drupal\qs_acl\Service\PrivilegeManager;
 
 /**
@@ -32,6 +33,13 @@ class UserController extends ControllerBase {
   protected $activityManager;
 
   /**
+   * The entity QS Photo Manager.
+   *
+   * @var \Drupal\qs_photo\Service\PhotoManager
+   */
+  protected $photoManager;
+
+  /**
    * The Privilege Manager.
    *
    * @var \Drupal\qs_acl\Service\PrivilegeManager
@@ -41,11 +49,12 @@ class UserController extends ControllerBase {
   /**
    * {@inheritdoc}
    */
-  public function __construct(AccessControl $acl, PrivilegeManager $privilege_manager, ActivityManager $activity_manager) {
+  public function __construct(AccessControl $acl, PrivilegeManager $privilege_manager, ActivityManager $activity_manager, PhotoManager $photo_manager) {
     $this->acl              = $acl;
     $this->privilegeManager = $privilege_manager;
     $this->nodeStorage      = $this->entityTypeManager()->getStorage('node');
     $this->activityManager  = $activity_manager;
+    $this->photoManager     = $photo_manager;
   }
 
   /**
@@ -57,7 +66,8 @@ class UserController extends ControllerBase {
     // Load customs services used in this class.
       $container->get('qs_acl.access_control'),
       $container->get('qs_acl.privilege_manager'),
-      $container->get('qs_activity.activity_manager')
+      $container->get('qs_activity.activity_manager'),
+      $container->get('qs_photo.photo_manager')
     );
   }
 
@@ -110,7 +120,17 @@ class UserController extends ControllerBase {
     else {
       // Show only activity where user has upload photo access &
       // with at least one past event.
-      $variables['activities'] = $this->activityManager->getByUserPhoto($community, $user, FALSE);
+      $variables['activities'] = $this->activityManager->getByUserPhoto($community, $user, TRUE);
+    }
+
+    // Get number of writable photos by activities.
+    // This may slow down the loading of this page.
+    foreach ($variables['activities'] as $activity) {
+      $photos = $this->photoManager->getWritablePhotoByUser($activity, $user);
+      $count_photos = count($photos);
+      if ($count_photos > 0) {
+        $variables['photos_by_activities'][$activity->id()] = $count_photos;
+      }
     }
 
     return [

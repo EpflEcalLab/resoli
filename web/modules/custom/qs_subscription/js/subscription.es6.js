cc8a25aea18c186c59df28040585ce1e5a297d25
Kevin Wenger <wenger.kev@gmail.com>
2019-03-20 11:57:21 +0100

    babelify the subscription.js which was uncompatible with IE or some Apple device - close #683

--- web/modules/custom/qs_subscription/js/subscription.es6.js
+++ web/modules/custom/qs_subscription/js/subscription.es6.js
@@ -1,13 +1,11 @@
-"use strict";
-
 /**
  * @file
  * Confirm modal dialog for specific ajax submits.
  * See https://www.drupal.org/project/ajax_confirm
  */
 (function ($, Drupal) {
-  var timer = null;
-  var delay = 2500;
+  let timer = null;
+  const delay = 2500;
 
   /**
    * Attaches the confirm dialog to all enabled elements for ajax confirmation.
@@ -20,8 +18,7 @@
    *   Detaches the autocomplete behaviors.
    */
   Drupal.behaviors.subscriptionConfirm = {
-    attach: function attach(context, settings) {
-
+    attach: function (context, settings) {
       /**
        * Defer the confirmation step.
        *
@@ -32,25 +29,39 @@
        */
       function confirmDeferred($el) {
         // This is pretty much the same as the form-ajax.js script.
-        var defer = $.Deferred();
-        var text = $el.text();
-        var $icon = $el.find('.icon').clone();
-        var confirmText = $el.data('confirm'); // Send request if we are in pending state
+        const defer = $.Deferred();
+        const text = $el.text();
+        const $icon = $el.find('.icon').clone();
+        const confirmText = $el.data('confirm');
 
+        // Send request if we are in pending state
         if ($el.data('pending')) {
           window.clearTimeout(timer);
-          $el.removeData('pending');
+          $el
+            .removeData('pending');
           defer.resolve('yes');
           return defer.promise();
-        } // Set the pending state.
+        }
 
+        // Set the pending state.
+        $el
+          .text(confirmText)
+          .prepend($icon)
+          .data('confirm', text)
+          .data('pending', 'true')
+          .addClass('btn-confirm');
 
-        $el.text(confirmText).prepend($icon).data('confirm', text).data('pending', 'true').addClass('btn-confirm'); // Set a timeout, after which we cancel the demand.
-
-        timer = window.setTimeout(function () {
-          $el.text(text).prepend($icon).data('confirm', confirmText).removeData('pending').removeClass('btn-confirm');
+        // Set a timeout, after which we cancel the demand.
+        timer = window.setTimeout(() => {
+          $el
+            .text(text)
+            .prepend($icon)
+            .data('confirm', confirmText)
+            .removeData('pending')
+            .removeClass('btn-confirm');
           defer.resolve('no');
         }, delay);
+
         return defer.promise();
       }
 
@@ -64,56 +75,54 @@
        *   The ajax instance if found, otherwise null.
        */
       function findAjaxInstance(element) {
-        var ajax = null;
-        var selector = "#".concat(element.id);
-
-        for (var index in Drupal.ajax.instances) {
-          var ajaxInstance = Drupal.ajax.instances[index];
-
-          if (ajaxInstance && ajaxInstance.selector === selector) {
+        let ajax = null;
+        const selector = `#${element.id}`;
+        for (const index in Drupal.ajax.instances) {
+          const ajaxInstance = Drupal.ajax.instances[index];
+          if (ajaxInstance && (ajaxInstance.selector === selector)) {
             ajax = ajaxInstance;
             break;
           }
         }
-
         return ajax;
       }
+
 
       /**
        * Confirm the subscription, avoid sending form if not ready yet.
        */
       function subscriptionConfirm(context, settings) {
         if (typeof settings.subscriptionConfirm !== 'undefined') {
-          $(context).find(".".concat(settings.subscriptionConfirm)).once('ajax-confirm').each(function () {
-            var ajax = findAjaxInstance(this);
-            var $this = $(this);
-
+          $(context).find(`.${settings.subscriptionConfirm}`).once('ajax-confirm').each(function () {
+            const ajax = findAjaxInstance(this);
+            const $this = $(this);
             if (ajax) {
               // Store the original beforeSend function, which will be called
               // if the user confirms the action.
-              ajax.options.originalBeforeSend = ajax.options.beforeSend; // Overwrite the original beforeSend function, so that we first
+              ajax.options.originalBeforeSend = ajax.options.beforeSend;
+
+              // Overwrite the original beforeSend function, so that we first
               // interrupt the ajax submit and then show confirmation button and
               // if the user confirms the action then the ajax action will be
               // triggered again and the original beforeSend function will be
               // called.
-
               ajax.options.beforeSend = function (xmlhttprequest, options) {
                 if (!ajax.alreadyConfirmed) {
                   // Wait for an user input and if desired trigger the ajax
                   // submission again but flag the ajax object so that the
                   // next time we do not interrupt the submission.
-                  confirmDeferred($this, settings.subscriptionConfirm).then(function (answer) {
+                  confirmDeferred($this, settings.subscriptionConfirm).then((answer) => {
                     if (answer === 'yes') {
                       ajax.alreadyConfirmed = true;
                       $(ajax.element).trigger(ajax.element_settings.event);
                     }
-                  }); // Interrupt the ajax submission.
-
+                  });
+                  // Interrupt the ajax submission.
                   ajax.ajaxing = false;
                   return false;
                 }
 
-                var beforeSend = ajax.options.originalBeforeSend(xmlhttprequest, options);
+                const beforeSend = ajax.options.originalBeforeSend(xmlhttprequest, options);
                 ajax.alreadyConfirmed = false;
                 return beforeSend;
               };
@@ -123,6 +132,6 @@
       }
 
       subscriptionConfirm(context, settings);
-    }
+    },
   };
-})(jQuery, Drupal);
+}(jQuery, Drupal));

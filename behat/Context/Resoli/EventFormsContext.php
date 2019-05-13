531a2180ac3fc64ff593769999ecf03f51ea248a
Kevin Wenger <wenger.kev@gmail.com>
2019-05-13 10:27:33 +0200

    add a context behat to fill the Edit form of Event

--- behat/Context/Resoli/EventFormsContext.php
+++ behat/Context/Resoli/EventFormsContext.php
@@ -46,4 +46,44 @@ class EventFormsContext extends RawDrupalContext implements SnippetAcceptingCont
     }
   }
 
+  /**
+   * Fill the Event Edit Form.
+   *
+   * @param string $community
+   *   The community slug of event to edit.
+   * @param string $activity
+   *   The activity's event slug to edit.
+   * @param string $event
+   *   The event slug to edit.
+   * @param \Behat\Gherkin\Node\TableNode $fields
+   *   The fields value to use.
+   *
+   * @Given I fill the Edit Event :event form on activity :activity of :community with:
+   */
+  public function fillEventEditForm($community, $activity, $event, TableNode $fields) {
+    $this->visitPath("/$community/activities/$activity/events/$event/edit");
+
+    foreach ($fields->getHash() as $field) {
+      $dt = new DrupalDateTime();
+      $dt->setTimezone(new DateTimeZone('UTC'));
+      $dt->modify($field['date']);
+
+      $this->getSession()->getPage()->fillField('edit-title', $field['title']);
+      $this->getSession()->getPage()->fillField('edit-date', $dt->format('d.m.Y'));
+      $this->getSession()->getPage()->fillField('edit-start-at', $field['start-at']);
+      $this->getSession()->getPage()->fillField('edit-end-at', $field['end-at']);
+      $this->getSession()->getPage()->fillField('edit-body', $field['body']);
+      $this->getSession()->getPage()->fillField('edit-venue', $field['venue']);
+      $this->getSession()->getPage()->fillField('edit-contact-name', $field['contact-name']);
+      $this->getSession()->getPage()->fillField('edit-contact-phone', $field['contact-phone']);
+      $this->getSession()->getPage()->fillField('edit-contact-mail', $field['contact-mail']);
+
+      $this->getSession()->getPage()->selectFieldOption('has_contribution', '0');
+      if (isset($field['contribution'])) {
+        $this->getSession()->getPage()->selectFieldOption('has_contribution', '1');
+        $this->getSession()->getPage()->fillField('edit-contribution', $field['contribution']);
+      }
+    }
+  }
+
 }

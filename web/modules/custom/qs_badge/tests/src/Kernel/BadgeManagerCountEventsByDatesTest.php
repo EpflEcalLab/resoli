3abb3eb1c83418527dec2c19bc06ebe49f953155
Kevin Wenger <wenger.kev@gmail.com>
2019-05-23 13:52:44 +0200

    improve covearge of BadgeManager::countEventsByDates

--- web/modules/custom/qs_badge/tests/src/Kernel/BadgeManagerCountEventsByDatesTest.php
+++ web/modules/custom/qs_badge/tests/src/Kernel/BadgeManagerCountEventsByDatesTest.php
@@ -12,8 +12,9 @@ use Drupal\Core\Datetime\DrupalDateTime;
  * @group qs_kernel
  * @group qs_badge
  * @group qs_badge_kernel
+ * @group kevin
  */
-class BadgeManagerTest extends ResoliKernelTestBase {
+class BadgeManagerCountEventsByDatesTest extends ResoliKernelTestBase {
 
   /**
    * Modules to enable.
@@ -77,29 +78,73 @@ class BadgeManagerTest extends ResoliKernelTestBase {
     $event = $this->entityTypeManager->getStorage('node')->create([
       'type' => 'event',
       'title' => $this->randomString(),
+      'field_activity' => $activities[2]->id(),
+      'field_start_at' => '2016-08-30T09:00:00',
+      'field_end_at' => '2016-08-30T09:30:00',
+    ]);
+    $event->save();
+
+    $start = DrupalDateTime::createFromFormat('Y-m-d', '2016-07-27');
+    $end = DrupalDateTime::createFromFormat('Y-m-d', '2017-09-29');
+
+    $events = $this->badgeManager->countEventsByDates($communities[1], $start, $end);
+    $this->assertInternalType('array', $events);
+    $this->assertSame([
+      '2016-09-28' => '2',
+    ], $events);
+  }
+
+  /**
+   * @covers ::countEventsByDates
+   */
+  public function testCountEventsByDates() {
+    $communities = $this->seedCommunities(1);
+    $activities = $this->seedActivities($communities[1], 1);
+
+    $event = $this->entityTypeManager->getStorage('node')->create([
+      'type' => 'event',
+      'title' => $this->randomString(),
       'field_activity' => $activities[1]->id(),
-      'field_start_at' => '2016-08-28T09:00:00',
-      'field_end_at' => '2016-08-28T09:30:00',
+      'field_start_at' => '2014-09-28T12:00:00',
+      'field_end_at' => '2014-09-28T16:30:00',
     ]);
     $event->save();
 
     $event = $this->entityTypeManager->getStorage('node')->create([
       'type' => 'event',
       'title' => $this->randomString(),
-      'field_activity' => $activities[2]->id(),
-      'field_start_at' => '2016-09-28T12:00:00',
-      'field_end_at' => '2016-09-28T16:30:00',
+      'field_activity' => $activities[1]->id(),
+      'field_start_at' => '2016-09-28T13:00:00',
+      'field_end_at' => '2016-09-28T14:00:00',
+    ]);
+    $event->save();
+
+    $event = $this->entityTypeManager->getStorage('node')->create([
+      'type' => 'event',
+      'title' => $this->randomString(),
+      'field_activity' => $activities[1]->id(),
+      'field_start_at' => '2019-08-28T09:00:00',
+      'field_end_at' => '2019-08-28T09:30:00',
+    ]);
+    $event->save();
+
+    $event = $this->entityTypeManager->getStorage('node')->create([
+      'type' => 'event',
+      'title' => $this->randomString(),
+      'field_activity' => $activities[1]->id(),
+      'field_start_at' => '2019-08-28T09:00:00',
+      'field_end_at' => '2019-08-28T09:30:00',
     ]);
     $event->save();
 
     $start = DrupalDateTime::createFromFormat('Y-m-d', '2016-07-27');
-    $end = DrupalDateTime::createFromFormat('Y-m-d', '2016-09-29');
+    $end = DrupalDateTime::createFromFormat('Y-m-d', '2020-09-29');
 
     $events = $this->badgeManager->countEventsByDates($communities[1], $start, $end);
     $this->assertInternalType('array', $events);
     $this->assertSame([
-      '2016-08-28' => '1',
-      '2016-09-28' => '2',
+      '2016-09-28' => '1',
+      '2019-08-28' => '2',
     ], $events);
   }
 

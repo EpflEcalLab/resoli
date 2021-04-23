976d07fbd3a7d1a9ac347d6029d0b5c2a078bdda
Iris Wolfisberg <iris.wolfisberg@protonmail.com>
2021-04-23 16:06:06 +0200

    Add translations to quill options

--- web/themes/quartiers_solidaires/assets/components/molecules/editor/qs-quill.js
+++ web/themes/quartiers_solidaires/assets/components/molecules/editor/qs-quill.js
@@ -4,18 +4,32 @@
 (function ($) {
   $(document).ready(function () {
     // Initialized Quill rich-text editor for each instance of ".editor"
-    $('.editor').each(function () {
+    $('.quill-editor').each(function () {
       const identifier = $(this).attr('id');
 
-      new Quill(`#${identifier}`, {
+      const quill = new Quill(`#${identifier}`, {
         modules: {
           toolbar: [
             [{ header: [1, 2, false] }],
             ['bold', 'italic', 'underline'],
           ]
         },
-        placeholder: 'Ecrire quelque chose...',
+        placeholder: Drupal.t('qs.quill.editor.placeholder'),
         theme: 'bubble'
+      });
+
+      // Translate the toolbar heading options
+      const h1 = Drupal.t('qs.quill.editor.h1');
+      const h2 = Drupal.t('qs.quill.editor.h2');
+      $(`#${identifier} .ql-picker-item[data-value="1"]`).attr('data-h1-translation', h1);
+      $(`#${identifier} .ql-picker-item[data-value="2"]`).attr('data-h2-translation', h2);
+      $(`#${identifier} .ql-picker.ql-header .ql-picker-label[data-value="1"]`).attr('data-h1-translation', h1);
+      $(`#${identifier} .ql-picker.ql-header .ql-picker-label[data-value="2"]`).attr('data-h2-translation', h2);
+
+      // Translate the current selected option
+      quill.on('text-change', function() {
+        $(`#${identifier} .ql-picker.ql-header .ql-picker-label[data-value="1"]`).attr('data-h1-translation', h1);
+        $(`#${identifier} .ql-picker.ql-header .ql-picker-label[data-value="2"]`).attr('data-h2-translation', h2);
       });
     })
   });

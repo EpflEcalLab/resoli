08679411c30986ca3f5a6e626d69a500bee3be0d
Kevin Wenger <wenger.kev@gmail.com>
2021-08-26 14:31:38 +0200

    add new Sharging entity 'Volunteerism'

---
+++ web/modules/custom/qs_sharing/src/Entity/Volunteerism.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Drupal\qs_sharing\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\taxonomy\TermInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the sharing Volunteerism entity.
+ *
+ * Created or deleted when a user become volunteer on a Community for a Themes.
+ *
+ * @ContentEntityType(
+ *     id="volunteerism",
+ *     label=@Translation("Volunteerism"),
+ *     handlers={
+ *         "view_builder": "Drupal\Core\Entity\EntityViewBuilder",
+ *         "views_data": "Drupal\views\EntityViewsData",
+ *         "form": {
+ *             "default": "Drupal\Core\Entity\ContentEntityForm",
+ *         },
+ *     },
+ *     base_table="volunteerisms",
+ *     admin_permission="administer volunteerism entity",
+ *     fieldable=false,
+ *     entity_keys={
+ *         "id": "id",
+ *     },
+ *     links={
+ *         "collection": "/admin/content/volunteerisms",
+ *     },
+ * )
+ */
+class Volunteerism extends ContentEntityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = [];
+
+    // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(new TranslatableMarkup('ID'))
+      ->setDescription(new TranslatableMarkup('The ID of the involvement as a volunteer.'))
+      ->setReadOnly(TRUE);
+
+    $fields['community'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(new TranslatableMarkup('Community'))
+      ->setDescription(new TranslatableMarkup('The community for which the user volunteers.'))
+      ->setRequired(TRUE)
+      ->setSetting('target_type', 'taxonomy_term');
+
+    $fields['user'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(new TranslatableMarkup('Volunteer'))
+      ->setDescription(new TranslatableMarkup('The user volunteering.'))
+      ->setRequired(TRUE)
+      ->setSetting('target_type', 'user');
+
+    $fields['theme'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(new TranslatableMarkup('Theme'))
+      ->setDescription(new TranslatableMarkup('The sharing theme for which the user is volunteering.'))
+      ->setRequired(TRUE)
+      ->setSetting('target_type', 'taxonomy_term');
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(new TranslatableMarkup('Created'))
+      ->setDescription(new TranslatableMarkup('When the request was created.'));
+
+    return $fields;
+  }
+
+  /**
+   * Get the community for which the user volunteers.
+   */
+  public function getCommunity() {
+    return $this->get('community');
+  }
+
+  /**
+   * Get the volunteering created timestamp.
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * Get the sharing theme for which the user is volunteering.
+   */
+  public function getTheme() {
+    return $this->get('theme');
+  }
+
+  /**
+   * Get the user volunteering.
+   */
+  public function getVolunteer() {
+    return $this->get('user');
+  }
+
+  /**
+   * Set the community for which the user volunteers.
+   */
+  public function setCommunity(TermInterface $community): self {
+    $this->set('community', $community);
+
+    return $this;
+  }
+
+  /**
+   * Set the sharing theme for which the user is volunteering.
+   */
+  public function setTheme(TermInterface $theme): self {
+    $this->set('theme', $theme);
+
+    return $this;
+  }
+
+  /**
+   * Set the user volunteering.
+   */
+  public function setVolunteer(UserInterface $account): self {
+    $this->set('user', $account->id());
+
+    return $this;
+  }
+
+}
---
+++ web/modules/custom/qs_sharing/tests/src/Kernel/VolunteerismRepositoryTest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Drupal\Tests\vevey_council_meeting\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\qs_test\TaxonomyTestTrait;
+use Drupal\Tests\field\Traits\EntityReferenceTestTrait;
+
+/**
+ * @coversDefaultClass \Drupal\qs_sharing\Repository\VolunteerismRepository
+ *
+ * @group qs
+ * @group qs_sharing
+ * @group qs_sharing_kernel
+ *
+ * @internal
+ */
+final class VolunteerismRepositoryTest extends KernelTestBase {
+  use EntityReferenceTestTrait;
+  use TaxonomyTestTrait;
+
+  /**
+   * The Entity Type Manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'user',
+    'taxonomy',
+    'field',
+    'qs_sharing',
+    'text',
+    'filter',
+    'system',
+  ];
+
+  /**
+   * The volunteerism repository.
+   *
+   * @var \Drupal\qs_sharing\Repository\VolunteerismRepository
+   */
+  protected $volunteerismRepository;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    /** @var \Drupal\Core\Entity\EntityTypeManager $entityTypeManager */
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+
+    $this->installSchema('system', ['sequences']);
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('taxonomy_term');
+    $this->setupTaxonomy();
+
+    $this->createVocabulary('sharing_themes');
+    $this->createVocabulary('community');
+
+    $this->installEntitySchema('volunteerism');
+
+    $this->volunteerismRepository = $this->container->get('qs_sharing.repository.volunteerism');
+  }
+
+  /**
+   * @covers ::getAllByUser
+   */
+  public function testGetAllByUserReturnsExpected(): void {
+    $fooUser = $this->entityTypeManager->getStorage('user')->create([
+      'mail' => 'foo@example.org',
+      'name' => 'foo@example.org',
+    ]);
+    $fooUser->save();
+
+    $barUser = $this->entityTypeManager->getStorage('user')->create([
+      'mail' => 'bar@example.org',
+      'name' => 'bar@example.org',
+    ]);
+    $barUser->save();
+
+    $community1 = $this->entityTypeManager->getStorage('taxonomy_term')->create([
+      'vid' => 'communities',
+      'name' => $this->randomString(),
+    ]);
+    $community1->save();
+
+    $community2 = $this->entityTypeManager->getStorage('taxonomy_term')->create([
+      'vid' => 'communities',
+      'name' => $this->randomString(),
+    ]);
+    $community2->save();
+
+    $theme1 = $this->entityTypeManager->getStorage('taxonomy_term')->create([
+      'vid' => 'sharing_themes',
+      'name' => 'Conviviality',
+    ]);
+    $theme1->save();
+
+    $theme2 = $this->entityTypeManager->getStorage('taxonomy_term')->create([
+      'vid' => 'sharing_themes',
+      'name' => 'Mobility',
+    ]);
+    $theme2->save();
+
+    $theme3 = $this->entityTypeManager->getStorage('taxonomy_term')->create([
+      'vid' => 'sharing_themes',
+      'name' => 'Digital',
+    ]);
+    $theme3->save();
+
+    $volunteerism1 = $this->entityTypeManager->getStorage('volunteerism')->create([
+      'theme' => $theme1,
+      'community' => $community1,
+      'user' => $fooUser,
+    ]);
+    $volunteerism1->save();
+
+    $volunteerism2 = $this->entityTypeManager->getStorage('volunteerism')->create([
+      'theme' => $theme2,
+      'community' => $community1,
+      'user' => $fooUser,
+    ]);
+    $volunteerism2->save();
+
+    $volunteerism3 = $this->entityTypeManager->getStorage('volunteerism')->create([
+      'theme' => $theme1,
+      'community' => $community1,
+      'user' => $barUser,
+    ]);
+    $volunteerism3->save();
+
+    $volunteerism4 = $this->entityTypeManager->getStorage('volunteerism')->create([
+      'theme' => $theme1,
+      'community' => $community2,
+      'user' => $barUser,
+    ]);
+    $volunteerism4->save();
+
+    $volunteerisms = $this->volunteerismRepository->getAllByCommunityUser($community1, $barUser);
+    self::assertCount(1, $volunteerisms);
+
+    $volunteerisms = $this->volunteerismRepository->getAllByCommunityUser($community2, $barUser);
+    self::assertCount(1, $volunteerisms);
+
+    $volunteerisms = $this->volunteerismRepository->getAllByCommunityUser($community1, $fooUser);
+    self::assertCount(2, $volunteerisms);
+
+    $volunteerisms = $this->volunteerismRepository->getAllByCommunityUser($community2, $fooUser);
+    self::assertNull($volunteerisms);
+  }
+
+}

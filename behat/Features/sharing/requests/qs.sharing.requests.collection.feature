5d4d3efb6ec5cc4e9d3923f5538ae9899d0a382d
Kevin Wenger <wenger.kev@gmail.com>
2021-09-10 16:16:51 +0200

    init requests controller with access checker

--- behat/Features/sharing/requests/qs.sharing.requests.collection.feature
+++ behat/Features/sharing/requests/qs.sharing.requests.collection.feature
@@ -9,3 +9,40 @@ Feature: Collection of Requests
     When I am on "/node/77"
     And the url should match "/sharing/1/requests"
     And the response status code should be 200
+
+## Access
+  @api
+  Scenario Outline: As anonymous I should not be able to access any community offers collection.
+    Given I am on "<url>"
+    And the response status code should be 403
+    Examples:
+      | url |
+      | /sharing/1/requests |
+      | /sharing/2/requests |
+      | /sharing/3/requests |
+
+  @api
+  Scenario Outline: Logged-in, I can access my own community(ies) request collection if I'm also Volunteer on that community. Accessing community in which I don't belongs should not be unauthorized.
+    Given I am logged in as user "<user>"
+    When I am on "<url>"
+    Then the response status code should be <code>
+    Examples:
+      | user | url | code |
+      | admin | /sharing/1/requests | 200 |
+      | admin | /sharing/2/requests | 200 |
+      | member+lausanne | /sharing/1/requests | 200 |
+      | member+lausanne | /sharing/2/requests | 403 |
+      | approval+lausanne | /sharing/1/requests | 403 |
+      | approval+lausanne | /sharing/2/requests | 403 |
+      | manager+lausanne | /sharing/1/requests | 403 |
+      | manager+lausanne | /sharing/2/requests | 403 |
+      | member+lausanne+organizer+fribourg | /sharing/1/requests | 200 |
+      | member+lausanne+organizer+fribourg | /sharing/2/requests | 200 |
+      | member+fribourg+approval+organizer+fribourg | /sharing/1/requests | 403 |
+      | member+fribourg+approval+organizer+fribourg | /sharing/2/requests | 403 |
+      | declined+organizer+lausanne | /sharing/1/requests | 403 |
+      | declined+organizer+lausanne | /sharing/2/requests | 403 |
+      | member+lausanne+declined+organizer+lausanne | /sharing/1/requests | 403 |
+      | member+lausanne+declined+organizer+lausanne | /sharing/2/requests | 403 |
+      | member+fribourg+declined+member+lausanne | /sharing/1/requests | 403 |
+      | member+fribourg+declined+member+lausanne | /sharing/2/requests | 403 |
---
+++ web/modules/custom/qs_sharing/src/Controller/RequestController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Drupal\qs_sharing\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\qs_acl\Service\AccessControl;
+use Drupal\qs_sharing\Repository\RequestRepository;
+use Drupal\taxonomy\TermInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Collection of Requests for Sharing.
+ */
+class RequestController extends ControllerBase {
+
+  /**
+   * Access Control Service.
+   *
+   * @var \Drupal\qs_acl\Service\AccessControl
+   */
+  private $acl;
+
+  /**
+   * The Request repository.
+   *
+   * @var \Drupal\qs_sharing\Repository\RequestRepository
+   */
+  private $requestRepository;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(AccessControl $acl, RequestRepository $request_repository) {
+    $this->acl = $acl;
+    $this->requestRepository = $request_repository;
+  }
+
+  /**
+   * Checks access.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   * @param \Drupal\taxonomy\TermInterface $community
+   *   Run access checks for this taxonomy.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function access(AccountInterface $account, TermInterface $community): AccessResultInterface {
+    $access = AccessResult::forbidden();
+
+    if ($this->acl->isCommunityVolunteer($community)) {
+      $access = AccessResult::allowed();
+    }
+
+    return $access;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+    // Load customs services used in this class.
+      $container->get('qs_acl.access_control'),
+      $container->get('qs_sharing.repository.request')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags(?array $nodes = NULL) {
+    $tags = [
+      // Invalidated whenever any Offer is updated, deleted or created.
+      'node_list:request',
+    ];
+
+    if ($nodes) {
+      foreach ($nodes as $node) {
+        $tags[] = 'node:' . $node->id();
+      }
+    }
+
+    return $tags;
+  }
+
+  /**
+   * Requests collection page.
+   */
+  public function requests(Request $request, TermInterface $community) {
+    $variables = [
+      'community' => $community,
+    ];
+
+    return [
+      '#theme' => 'qs_sharing_collection_requests_page',
+      '#variables' => $variables,
+      '#cache' => [
+        'tags' => $this->getCacheTags(),
+        'contexts' => [
+          'user',
+        ],
+      ],
+    ];
+  }
+
+}

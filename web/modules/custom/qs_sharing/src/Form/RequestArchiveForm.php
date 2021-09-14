2c8d08992e00b5a7f2c49a1239c28f115fe0f629
Kevin Wenger <wenger.kev@gmail.com>
2021-09-14 16:59:13 +0200

    add RequestArchiveForm

---
+++ web/modules/custom/qs_sharing/src/Form/RequestArchiveForm.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Drupal\qs_sharing\Form;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\node\NodeInterface;
+use Drupal\qs_acl\Service\AccessControl;
+use Drupal\qs_sharing\Manager\RequestManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Form to archive a request by the current logged-in user.
+ */
+class RequestArchiveForm extends FormBase {
+
+  /**
+   * The node Storage.
+   *
+   * @var \Drupal\node\NodeStorageInterface
+   */
+  protected $nodeStorage;
+
+  /**
+   * The user Storage.
+   *
+   * @var \Drupal\user\UserStorageInterface
+   */
+  protected $userStorage;
+
+  /**
+   * Access Control Service.
+   *
+   * @var \Drupal\qs_acl\Service\AccessControl
+   */
+  private $acl;
+
+  /**
+   * The Offer Manager.
+   *
+   * @var \Drupal\qs_sharing\Manager\RequestManager
+   */
+  private $requestManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(AccessControl $acl, EntityTypeManagerInterface $entity_type_manager, RequestManager $request_manager) {
+    $this->acl = $acl;
+    $this->nodeStorage = $entity_type_manager->getStorage('node');
+    $this->userStorage = $entity_type_manager->getStorage('user');
+    $this->requestManager = $request_manager;
+  }
+
+  /**
+   * Checks access.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   * @param \Drupal\node\NodeInterface $request
+   *   Run access checks for this node.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function access(AccountInterface $account, NodeInterface $request) {
+    $access = AccessResult::forbidden();
+
+    if ($this->acl->hasWriteAccessRequest($request) && $request->get('moderation_state')->value !== 'archived') {
+      $access = AccessResult::allowed();
+    }
+
+    return $access;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $options = NULL) {
+    if (!isset($options['request'])) {
+      return $form;
+    }
+
+    /** @var \Drupal\node\NodeInterface $request */
+    $request = $options['request'];
+
+    // Save the request for later usage on submission.
+    $form_state->set('request', $request->id());
+
+    // Disable caching.
+    $form['#cache']['max-age'] = 0;
+
+    $form['#attributes'] = [
+      'data-confirm' => 'true',
+      'data-parent' => 'card' . $request->id(),
+      'class' => [
+        'request',
+        'request' . $request->id(),
+        'archive',
+        'mx-auto',
+        'mb-3',
+      ],
+    ];
+    $form['action']['submit'] = [
+      '#type' => 'submit',
+      '#name' => 'submit',
+      '#value' => $this->t('qs_sharing.collection.request.archive'),
+      '#attributes' => [
+        'icon' => 'trash',
+        'icon_left' => TRUE,
+        'class' => [
+          'btn',
+          'btn-outline-invert',
+          'btn-icon',
+          'shadow-to-bottom',
+          'btn-block',
+          'bg-danger',
+        ],
+        'data-confirm' => $this->t('qs_sharing.collection.request.archive.confirmed'),
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('qs_acl.access_control'),
+      $container->get('entity_type.manager'),
+      $container->get('qs_sharing.manager.request')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'qs_sharing_request_archive_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /** @var \Drupal\node\NodeInterface $request */
+    $request = $this->nodeStorage->load($form_state->get('request'));
+
+    // Archive the request.
+    $this->requestManager->archive($request);
+
+    // When archived by someone else than the request author, send an email.
+    if ($this->currentUser()->id() !== $request->get('uid')->target_id) {
+      $currentUser = $this->userStorage->load($this->currentUser()->id());
+      $this->requestManager->sendArchivedMail($request, $currentUser);
+    }
+
+    $this->messenger()->addMessage($this->t('qs_sharing.collection.request.archive.success'));
+
+    $form_state->setRedirect('qs_sharing.collection.request', [
+      'community' => $request->field_community->target_id,
+    ]);
+  }
+
+}

e5d442370f5262dfc97cc701e5635d8552422c36
Kevin Wenger <wenger.kev@gmail.com>
2021-09-15 11:11:46 +0200

    init the add request form with inuts

--- behat/Features/sharing/requests/qs.sharing.requests.form.add.feature
+++ behat/Features/sharing/requests/qs.sharing.requests.form.add.feature
@@ -1,15 +1,84 @@
+@debug
 Feature: Sharing Request add Form
 
+## Access
+  Scenario Outline: The add Request form is only available to member of the community.
+    Given I am logged in as user "<user>"
+    When I am on "<url>"
+    Then the response status code should be <code>
+    Examples:
+      | user | url | code |
+      | admin | /sharing/1/requests/add | 200 |
+      | admin | /sharing/2/requests/add | 200 |
+      | admin | /sharing/3/requests/add | 200 |
+      | member+lausanne | /sharing/1/requests/add | 200 |
+      | member+lausanne | /sharing/2/requests/add | 403 |
+      | member+lausanne | /sharing/3/requests/add | 403 |
+      | approval+lausanne | /sharing/1/requests/add | 403 |
+      | approval+lausanne | /sharing/2/requests/add | 403 |
+      | approval+lausanne | /sharing/3/requests/add | 403 |
+      | manager+lausanne | /sharing/1/requests/add | 200 |
+      | manager+lausanne | /sharing/2/requests/add | 403 |
+      | manager+lausanne | /sharing/3/requests/add | 403 |
+      | organizer+lausanne | /sharing/1/requests/add | 200 |
+      | organizer+lausanne | /sharing/2/requests/add | 403 |
+      | organizer+lausanne | /sharing/3/requests/add | 403 |
+      | member+lausanne+organizer+fribourg | /sharing/1/requests/add | 200 |
+      | member+lausanne+organizer+fribourg | /sharing/2/requests/add | 200 |
+      | member+lausanne+organizer+fribourg | /sharing/3/requests/add | 403 |
+      | member+fribourg+approval+organizer+fribourg | /sharing/1/requests/add | 403 |
+      | member+fribourg+approval+organizer+fribourg | /sharing/2/requests/add | 200 |
+      | member+fribourg+approval+organizer+fribourg | /sharing/3/requests/add | 403 |
+      | member+fribourg+organizer+fribourg | /sharing/1/requests/add | 403 |
+      | member+fribourg+organizer+fribourg | /sharing/2/requests/add | 200 |
+      | member+fribourg+organizer+fribourg | /sharing/3/requests/add | 403 |
+      | approval+member+fribourg+approval+organizer+fribourg | /sharing/1/requests/add | 403 |
+      | approval+member+fribourg+approval+organizer+fribourg | /sharing/2/requests/add | 403 |
+      | approval+member+fribourg+approval+organizer+fribourg | /sharing/3/requests/add | 403 |
+      | declined+organizer+lausanne | /sharing/1/requests/add | 403 |
+      | declined+organizer+lausanne | /sharing/2/requests/add | 403 |
+      | declined+organizer+lausanne | /sharing/3/requests/add | 403 |
+      | member+lausanne+declined+organizer+lausanne | /sharing/1/requests/add | 200 |
+      | member+lausanne+declined+organizer+lausanne | /sharing/2/requests/add | 403 |
+      | member+lausanne+declined+organizer+lausanne | /sharing/3/requests/add | 403 |
+      | member+fribourg+declined+member+lausanne | /sharing/1/requests/add | 403 |
+      | member+fribourg+declined+member+lausanne | /sharing/2/requests/add | 200 |
+      | member+fribourg+declined+member+lausanne | /sharing/3/requests/add | 403 |
+
 ## Floating Button
-  @api
-  Scenario: In the Sharing request form page, I don't see any floating button
-    Given I am logged in as user "admin"
-    When I am on "/sharing/1/request/add"
-    Then I should see 0 ".floating a" elements
+  Scenario Outline: In the Sharing request form page, I should see the floating button pointing to my dashboard.
+    Given I am logged in as user "<user>"
+    When I am on "/sharing/1/requests/add"
+    Then I should see 1 ".floating a" elements
+    And I should see "qs_sharing.floating.dashboard" link with href "/sharing/1/user/<user-id>/dashboard"
+    Examples:
+      | user | user-id |
+      | member+lausanne | 2 |
+      | manager+lausanne | 5 |
+      | organizer+lausanne | 6 |
+      | member+lausanne+manager+fribourg | 13 |
 
 # Back button.
-  @api
   Scenario: In the Sharing request form page, I don't see any back button
     Given I am logged in as user "admin"
-    When I am on "/sharing/1/request/add"
+    When I am on "/sharing/1/requests/add"
     Then I should not see a "#block-previousnavigation a" element
+
+## Form pre-filled values.
+  Scenario Outline: In the add Request form, the contact information must be prefilled using logged-in account data.
+    Given I am volunteer on community <community> for theme 22 as user <user_id>
+    Given I am logged in as user "<user>"
+    When I am on "/sharing/<community>/requests/add"
+    And the "edit-contact-firstname" field should contain "<firstname>"
+    And the "edit-contact-lastname" field should contain "<lastname>"
+    And the "edit-contact-phone" field should contain "<phone>"
+    And the "edit-contact-mail" field should contain "<mail>"
+    Examples:
+      | user | user_id | community | firstname | lastname | phone | mail |
+      | admin | 1 | 1 | | | | dev@antistatique.net |
+      | member+lausanne | 2 | 1 | Sarah | Courci | 0211234567 | member+lausanne@antistatique.net |
+      | manager+lausanne | 5 | 1 | Juda | Bricot | | manager+lausanne@antistatique.net |
+      | organizer+lausanne | 6 | 1 | Gerard | Mensoif | | organizer+lausanne@antistatique.net |
+      | member+lausanne+organizer+fribourg | 8 | 1 | Jerry | Kan | | member+lausanne+organizer+fribourg@antistatique.net |
+      | member+lausanne+organizer+fribourg | 8 | 2 | Jerry | Kan | | member+lausanne+organizer+fribourg@antistatique.net |
+      | member+fribourg+approval+organizer+fribourg | 14 | 2 | Paul | Honet | 0211234567 | member+fribourg+approval+organizer+fribourg@antistatique.net |
--- web/modules/custom/qs_sharing/src/Form/RequestAddForm.php
+++ web/modules/custom/qs_sharing/src/Form/RequestAddForm.php
@@ -3,19 +3,28 @@
 namespace Drupal\qs_sharing\Form;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Language\LanguageManager;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
 use Drupal\qs_acl\Service\AccessControl;
+use Drupal\qs_sharing\Manager\RequestManager;
 use Drupal\taxonomy\TermInterface;
-use Drupal\taxonomy\TermStorageInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Form to create a new sharing request in a community.
  */
 class RequestAddForm extends FormBase {
+
+  /**
+   * Access Control Service.
+   *
+   * @var \Drupal\qs_acl\Service\AccessControl
+   */
+  protected $acl;
   /**
    * The language manager.
    *
@@ -24,26 +33,55 @@ class RequestAddForm extends FormBase {
   protected $languageManager;
 
   /**
-   * Access Control Service.
+   * The node Storage.
    *
-   * @var \Drupal\qs_acl\Service\AccessControl
+   * @var \Drupal\node\NodeStorageInterface
    */
-  private $acl;
+  protected $nodeStorage;
+
+  /**
+   * The Request Manager.
+   *
+   * @var \Drupal\qs_sharing\Manager\RequestManager
+   */
+  protected $requestManager;
 
   /**
    * The term Storage.
    *
    * @var \Drupal\taxonomy\TermStorageInterface
    */
-  private $termStorage;
+  protected $termStorage;
 
   /**
-   * {@inheritdoc}
+   * The user Storage.
+   *
+   * @var \Drupal\user\UserStorageInterface
    */
-  public function __construct(AccessControl $acl, LanguageManager $language_manager, TermStorageInterface $termStorage) {
+  protected $userStorage;
+
+  /**
+   * Construct a new form allowing submission of Offers from Volunteers.
+   *
+   * @param \Drupal\qs_acl\Service\AccessControl $acl
+   *   The access controls.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   * @param \Drupal\qs_sharing\Manager\RequestManager $request_manager
+   *   The request manager.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function __construct(AccessControl $acl, EntityTypeManagerInterface $entity_type_manager, LanguageManager $language_manager, RequestManager $request_manager) {
     $this->acl = $acl;
     $this->languageManager = $language_manager;
-    $this->termStorage = $termStorage;
+    $this->termStorage = $entity_type_manager->getStorage('taxonomy_term');
+    $this->userStorage = $entity_type_manager->getStorage('user');
+    $this->nodeStorage = $entity_type_manager->getStorage('node');
+    $this->requestManager = $request_manager;
   }
 
   /**
@@ -73,6 +111,7 @@ class RequestAddForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state, ?TermInterface $community = NULL) {
     // Get the current language.
     $currentLang = $this->languageManager->getCurrentLanguage();
+    $account = $this->userStorage->load($this->currentUser()->id());
 
     // Disable caching.
     $form['#cache']['max-age'] = 0;
@@ -86,9 +125,12 @@ class RequestAddForm extends FormBase {
 
     $form['#floating_buttons'][] = [
       'icon' => 'sharing',
-      'label' => $this->t('qs_sharing.floating.my_requests'),
-      'active' => TRUE,
+      'label' => $this->t('qs_sharing.floating.dashboard'),
       'theme' => 'primary',
+      'url' => Url::fromRoute('qs_sharing.sharing.dashboard', [
+        'community' => $community->id(),
+        'user' => $this->currentUser()->id(),
+      ]),
     ];
 
     $form['#top_lead'] = $this->t('qs.sharing.add.request @community', ['@community' => $community->getName()]);
@@ -116,11 +158,43 @@ class RequestAddForm extends FormBase {
       ],
     ];
 
+    // Get all sharing themes for options.
+    $themes = $this->termStorage->loadTree('sharing_themes', 0, NULL, TRUE);
+    $options = [];
+
+    foreach ($themes as $theme) {
+      // Check if has translation.
+      if ($theme->hasTranslation($currentLang->getId())) {
+        $theme = $theme->getTranslation($currentLang->getId());
+      }
+      $options[$theme->id()] = $theme->getName();
+    }
+
+    $form['request']['step-1']['theme'] = [
+      '#title' => $this->t('qs_sharing.requests.form.add.theme'),
+      '#type' => 'select',
+      '#required' => FALSE,
+      '#options' => $options,
+      '#attributes' => [
+        'placeholder' => $this->t('qs_sharing.requests.form.add.theme.placeholder'),
+      ],
+    ];
+
+    $form['request']['#attached']['library'][] = 'quartiers_solidaires/quill';
     $form['request']['step-1']['body'] = [
       '#title' => $this->t('qs_sharing.requests.form.add.body'),
-      '#placeholder' => $this->t('qs_sharing.requests.form.add.body.placeholder'),
       '#type' => 'textarea',
       '#required' => FALSE,
+    ];
+    $form['request']['step-1']['quill_body'] = [
+      '#markup' => '<div class="form-group">
+        <span class="quill-label">' . $this->t('qs_sharing.requests.form.add.body') . '</span>
+        <div
+          id="editor-add-offer-body"
+          data-placeholder-translation="' . $this->t('qs_sharing.requests.form.add.body.placeholder') . '"
+          class="quill-editor quill-editor-primary form-textarea form-control">
+        </div>
+      </div>',
     ];
 
     $form['request']['step-2'] = [
@@ -135,17 +209,72 @@ class RequestAddForm extends FormBase {
       ],
     ];
 
-    // Get all themes for options.
-    $themes = $this->termStorage->loadTree('themes', 0, NULL, TRUE);
-    $options = [];
+    $form['request']['step-2']['contact_firstname'] = [
+      '#attributes' => [
+        'icon' => 'user',
+      ],
+      '#title' => $this->t('qs_sharing.requests.form.add.contact_firstname'),
+      '#placeholder' => $this->t('qs_sharing.requests.form.add.contact_firstname.placeholder'),
+      '#default_value' => $account->field_firstname->value,
+      '#type' => 'textfield',
+      '#required' => FALSE,
+      '#theme_wrappers' => [
+        'form_element',
+      ],
+    ];
 
-    foreach ($themes as $theme) {
-      // Check if has translation.
-      if ($theme->hasTranslation($currentLang->getId())) {
-        $theme = $theme->getTranslation($currentLang->getId());
-      }
-      $options[$theme->id()] = $theme->getName() . '|' . $theme->field_icon->value;
-    }
+    $form['request']['step-2']['contact_lastname'] = [
+      '#attributes' => [
+        'icon' => '_',
+      ],
+      '#placeholder' => $this->t('qs_sharing.requests.form.add.contact_lastname.placeholder'),
+      '#type' => 'textfield',
+      '#default_value' => $account->field_lastname->value,
+      '#required' => FALSE,
+      '#theme_wrappers' => [
+        'form_element',
+      ],
+    ];
+
+    $form['request']['step-2']['contact_mail'] = [
+      '#attributes' => [
+        'icon' => 'mail',
+      ],
+      '#title' => $this->t('qs_sharing.requests.form.add.contact_mail'),
+      '#placeholder' => $this->t('qs_sharing.requests.form.add.contact_mail.placeholder'),
+      '#default_value' => $account->mail->value,
+      '#type' => 'textfield',
+      '#required' => FALSE,
+      '#theme_wrappers' => [
+        'form_element',
+      ],
+    ];
+
+    $form['request']['step-2']['contact_phone'] = [
+      '#attributes' => [
+        'icon' => 'phone',
+      ],
+      '#placeholder' => $this->t('qs_sharing.requests.form.add.contact_phone.placeholder'),
+      '#default_value' => $account->field_phone->value,
+      '#type' => 'textfield',
+      '#required' => FALSE,
+      '#theme_wrappers' => [
+        'form_element',
+      ],
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#attributes' => [
+        'icon' => 'check',
+        'modal' => TRUE,
+        'outline' => TRUE,
+        'class' => [
+          'shadow-to-bottom',
+        ],
+      ],
+      '#value' => $this->t('qs_sharing.requests.form.add.save'),
+    ];
 
     return $form;
   }
@@ -156,8 +285,9 @@ class RequestAddForm extends FormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('qs_acl.access_control'),
+      $container->get('entity_type.manager'),
       $container->get('language_manager'),
-      $container->get('entity_type.manager')->getStorage('taxonomy_term')
+      $container->get('qs_sharing.manager.request')
     );
   }
 

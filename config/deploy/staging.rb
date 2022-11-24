4e4cd17439b33ab4cbf6feae924c0d9c3cc0a1f7
Kevin Wenger <wenger.kev@gmail.com>
2022-11-24 09:32:32 +0100

    change php verson when deploying staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -21,9 +21,15 @@ set :slackistrano, false
 before "styleguide:build_local", "httpauth:protect"
 before "styleguide:deploy_build", "styleguide:build_local" unless ENV['CI']
 
+# Map php command in order to change PHP version from "default" setup-one
+# See https://help.alwaysdata.com/fr/langages/php/probl%C3%A8mes-fr%C3%A9quents/#utiliser-diff%C3%A9rentes-versions-en-ssh
+# set :php, '/usr/bin/php'
+set :php, '/usr/bin/php8.0 -c /home/quartiers-solidaires/admin/config/php/php-497349.ini'
+SSHKit.config.command_map[:php] = -> { fetch(:php, 'php') }
+
 # Map composer and drush commands
 # NOTE: If stage have different deploy_to
 # you have to copy those line for each <stage_name>.rb
 # See https://github.com/capistrano/composer/issues/22
-SSHKit.config.command_map[:composer] = -> { shared_path.join('composer.phar') }
-SSHKit.config.command_map[:drush] = -> { release_path.join('vendor/bin/drush') }
+SSHKit.config.command_map[:composer] = -> { fetch(:php, 'php') + ' ' + shared_path.join('composer.phar').to_s }
+SSHKit.config.command_map[:drush] = -> { fetch(:php, 'php') + ' ' + release_path.join('vendor/bin/drush').to_s }

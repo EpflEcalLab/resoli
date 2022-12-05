cb236681dda8a465fc4623261dd5399fb4fe581e
Kevin Wenger <wenger.kev@gmail.com>
2022-12-05 09:28:20 +0100

    use Alwaysdata default PHP to deploy staging (PHP 8.0)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -23,8 +23,8 @@ before "styleguide:deploy_build", "styleguide:build_local" unless ENV['CI']
 
 # Map php command in order to change PHP version from "default" setup-one
 # See https://help.alwaysdata.com/fr/langages/php/probl%C3%A8mes-fr%C3%A9quents/#utiliser-diff%C3%A9rentes-versions-en-ssh
-# set :php, '/usr/bin/php'
-set :php, '/usr/bin/php8.0 -c /home/quartiers-solidaires/admin/config/php/php-497349.ini'
+set :php, '/usr/bin/php'
+# set :php, '/usr/bin/php8.0 -c /home/quartiers-solidaires/admin/config/php/php-497349.ini'
 SSHKit.config.command_map[:php] = -> { fetch(:php, 'php') }
 
 # Map composer and drush commands

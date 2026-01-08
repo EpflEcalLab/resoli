f38b41e3c6d4499ad524e38a8561a533518f9967
Kevin Wenger <wenger.kev@gmail.com>
2026-01-08 11:39:43 +0100

    update to Ruby v3 & latests Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-# config valid only for current version of Capistrano
-lock '3.17.1'
-
 set :application, 'quartiers-solidaires'
 set :repo_url, 'git@github.com:antistatique/quartiers-solidaires.git'
 
